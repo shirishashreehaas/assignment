8cbdc417f83cd251855ec0f9ef85fd652293418d
shirishavenkat <venkat4451@gmail.com>
2024-12-26 13:08:52 +0530

    Initial Commit

---
+++ CocktailApp/CocktailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailAppApp.swift
+//  CocktailApp
+//
+//  Created by Venkat_test on 26/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CocktailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
